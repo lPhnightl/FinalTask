0f10d7a3639907bdd1d33656933c3bd763cdb982
Alldor <lphnightl@gmail.com>
2021-12-28 21:26:24 +0300

    Finish task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,36 @@
 ﻿
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 20);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+int[] FilterArray(int[] array)
+{
+    int[] arrOut = new int[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) { arrOut[j] = array[i]; j++;}
+    }
+    Array.Resize(ref arrOut, j);
+    return arrOut;
+}
+
+int[] arrTask = new int[20];
+arrTask = FillArray(arrTask);
+Console.WriteLine("Изначальный массив:");
+PrintArray(arrTask);
+Console.WriteLine();
+Console.WriteLine("Отфильтрованный массив:");
+PrintArray(FilterArray(arrTask));
